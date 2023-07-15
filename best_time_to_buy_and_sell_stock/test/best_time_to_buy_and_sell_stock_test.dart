a033869517ac243327103429fdcb0788c63a4b4a
rim4j <amirhosseinjalali818@gmail.com>
2023-07-15 11:18:33 +0330

    best time to buy and sell stock

---
+++ best_time_to_buy_and_sell_stock/test/best_time_to_buy_and_sell_stock_test.dart
@@ -0,0 +1,8 @@
+import 'package:best_time_to_buy_and_sell_stock/best_time_to_buy_and_sell_stock.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
